762bb48ddf48f5618ab224f259ead9508ad402ef
UI <UI@UI>
2024-09-30 20:49:17 +0530

    Add project files.

---
+++ Model/SumRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class SumRequest
+    {
+        public List<int> Numbers { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ SumOfOddEven/Controllers/SumOfOddEvenController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.Runtime.Serialization;
+
+namespace SumOfOddEven.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SumOfOddEvenController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult GetSum(SumRequest request)
+        {
+            if (request == null || request.Numbers == null || !request.Numbers.Any())
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+            int sum;
+            if (request.Type.ToLower() == "odd")
+            {
+                sum = request.Numbers.Where(n => n % 2 != 0).Sum();
+                return StatusCode(StatusCodes.Status201Created, new { Result = sum });
+            }
+            else if (request.Type.ToLower() == "even")
+            {
+                sum = request.Numbers.Where(n => n % 2 == 0).Sum();
+                return StatusCode(StatusCodes.Status201Created, new { Result = sum });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+        }
+
+    }
+}
